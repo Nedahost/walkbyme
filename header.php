ded64b56ed174465752eef4d825c3751cf0bf310
Nedahost <aggelos13227@gmail.com>
2023-12-09 18:55:39 +0200

    change

--- header.php
+++ header.php
@@ -9,20 +9,9 @@
         <meta name="facebook-domain-verification" content="hywzavqq5x79jm858z7t71fq8gur2m" />
         <title><?php wp_title( '|', true, 'right' ); ?></title>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto+Flex:wght@200&display=swap" rel="stylesheet">
-        <?php wp_head(); ?>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5QBSL8G');</script>
-        <!-- End Google Tag Manager -->
+        <?php wp_head(); ?> 
     </head>
     <body>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QBSL8G"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         <header>
             <div class="wrapper"><!-- wrapper start -->
                 <div class="shopdetails"><!-- shop details start -->
