7b7363f368490fe9d82f4660cad6f2574309aae5
Nedahost <aggelos13227@gmail.com>
2024-09-10 21:58:06 +0300

    ly

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,67 +1,54 @@
-jQuery(document).on("ready", function() {
-        
-        
-        jQuery(".carousel").slick({
-        dots: false,
-        arrows:true,
-        autoplay:false,
-        infinite: false,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: false
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: false,
-            arrows:false
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-        });
-
-        jQuery(".slideshow").slick({
-          dots: true,
-          arrows:true,
-          autoplay:false,
-          infinite: false,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          responsive: [
-            {
+jQuery(document).ready(function($) {
+  const slickOptions = {
+      dots: false,
+      arrows: true,
+      autoplay: false,
+      infinite: false,
+      responsive: [
+          {
+              breakpoint: 1024,
+              settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: false
+              }
+          },
+          {
+              breakpoint: 600,
+              settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+              }
+          },
+          {
               breakpoint: 480,
               settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: false,
-                arrows:false
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  dots: false,
+                  arrows: false
               }
-            }
-          ]
-          });
-        
-});
+          }
+      ]
+  };
 
+  $(".carousel").slick({
+      ...slickOptions,
+      slidesToShow: 4,
+      slidesToScroll: 4
+  });
+
+  $(".slideshow").slick({
+      ...slickOptions,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+  });
+  $(".slideshow, .carousel").on('init', function(event, slick){
+    $(this).addClass('slick-initialized');
+});
+});
 
 
 function openSearchModal() {
